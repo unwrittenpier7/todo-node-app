ffca803e616cb372029ed1bea85a8a460efd1e5d
unwrittenpier7 <itsmepeter143@gmail.com>
2025-06-17 21:44:10 +0530

    updated

--- models/todoModel.js
+++ models/todoModel.js
@@ -1,6 +1,18 @@
-import mongoose from 'mongoose'
-const todoSchema = mongoose.Schema({
-  task: { type: String },
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export default mongoose.model("Todo", todoSchema);
